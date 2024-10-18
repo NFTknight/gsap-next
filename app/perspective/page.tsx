d8f111f2ca035153b7e3a5023b59949bc4a95020
NFT Knight <117064821+NFTknight@users.noreply.github.com>
2024-10-18 10:44:34 +0900

    update cursor size

--- app/perspective/page.tsx
+++ app/perspective/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
   const cursorRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const cursorSize = 16;
+    const cursorSize = 32;
     const handleMouseMove = (e: MouseEvent) => {
       const x = e.clientX - cursorSize / 2;
       const y = e.clientY - cursorSize / 2;
@@ -37,7 +37,7 @@ export default function HomePage() {
     <>
       <div
         ref={cursorRef}
-        className="w-4 h-4 bg-blue-500 rounded-full pointer-events-none"
+        className="w-8 h-8 bg-blue-500 rounded-full pointer-events-none"
       ></div>
       <div className="h-screen flex justify-center items-center overflow-hidden bg-blue-600">
         <div className="relative perspective-100 uppercase">
